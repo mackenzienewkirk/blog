6fadefeb055fc52fc99b7993fcdc3cf38389b2e6
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-05-01 08:19:56 -0500

    adding MUI card functions

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import { Card, colors } from '@mui/material';
-import shadows from '@mui/material/styles/shadows';
 
 function UserPage() {
+
+
+  
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <Card>
+      <Card >
       <h1>POST</h1>
       </Card>
     </div>
