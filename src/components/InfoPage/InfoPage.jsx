3173d7b2aeb23ad23ea4f658dfef7aa49c4c91ba
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-05-27 08:51:18 -0500

    updating info page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 function InfoPage() {
   return (
     <div className="container">
-      <p>Info Page</p>
+      <h1>Navigating through the changes.</h1>
     </div>
   );
 }
