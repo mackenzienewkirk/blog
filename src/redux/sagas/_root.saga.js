cfe69341be0ed0e1efd8cb62074bc97e8ca3e436
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-06-02 11:59:00 -0500

    updating root saga and post saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,7 +2,6 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
-import postSaga from './post.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -15,7 +14,6 @@ export default function* rootSaga() {
   yield all([
     loginSaga(), // login saga is now registered
     registrationSaga(),
-    userSaga(),
-    postSaga()
+    userSaga()
   ]);
 }
---
+++ src/redux/sagas/post.saga.js
@@ -0,0 +1,7 @@
+// import axios from "axios";
+
+// function postSaga() {
+//  yield takeLatest('POST', post);
+// }
+
+// export default postSaga;
