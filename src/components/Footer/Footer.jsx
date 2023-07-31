65c74ef0adf81fd23aed7584104b0603e85cc22c
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-07-31 11:36:02 -0500

    updating footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; EmmaMurano</footer>;
+  return <footer>&copy; KN</footer>;
 }
 
 export default Footer;
